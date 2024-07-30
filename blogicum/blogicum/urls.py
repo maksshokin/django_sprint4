8483354b01dc2b27ed218331459a5bf07ecd7e34
Maksim Shokin <maksshokin90@gmail.com>
2024-07-30 20:54:21 +0300

    yuhi

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -22,7 +22,10 @@ urlpatterns: list = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
 handler403 = 'pages.views.csrf_failure'
 handler404 = 'pages.views.page_not_found'
